396c056d2117c3ddae3bdea4f61c0eb53d28380e
vamsi <vamsikrishna.123.vk@gmail.com>
2022-10-18 14:59:47 +0530

    added the promote_release file

---
+++ promoterelease.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent any
+    triggers {pollscm ('* 17 * * 1-5')}
+    stages{
+        stage ('git clone') {
+            steps {
+                git branch : 'release', url : 'git@github.com:vamsibakka/shopizer.git'
+            }
+        }
+        stage ('merge') {
+            steps{
+                sh 'git merge develop --no-ff' # merging the git develop branch to release branch without fastfarward.
+                sh 'mvn clean package'
+            }
+        }
+        stage ('junit_testresults') {
+            steps {
+                junit '**/surefire-reports/*.xml'
+            }
+        }
+        stage ('archives') {
+            steps{
+                archiveArtifacts artifacts: '**/target/*.jar'
+            }
+            }
+        }
+    }

beaf4b6bf16a3942e8bee2461ab04831e15528c9
vamsi <vamsikrishna.123.vk@gmail.com>
2022-10-19 12:32:06 +0530

    only merge

--- promoterelease.groovy
+++ promoterelease.groovy
@@ -2,21 +2,28 @@ pipeline {
     agent any
     triggers { pollSCM ('H 17 * * 1-5')}
     stages{
+        stag {
+            steps {
+                
+                git branch : 'master', url: 'https://github.com/vamsibakka/shopizer.git'
+            }
+        }
                 stage ('merge') {
             steps{
+                sh 'git checkout release'
                 sh 'git merge develop --no-ff'  // merging the git develop branch to release branch without fastfarward.
-                sh 'mvn clean package'
+                //sh 'mvn clean package'
             }
         }
-        stage ('junit_testresults') {
-            steps {
-                junit '**/surefire-reports/*.xml'
-            }
-        }
-        stage ('archives') {
-            steps{
-                archiveArtifacts artifacts: '**/target/*.jar'
-            }
-            }
+        //stage ('junit_testresults') {
+         //   steps {
+           //     junit '**/surefire-reports/*.xml'
+            //}
+        //}
+        //stage ('archives') {
+          //  steps{
+            //    archiveArtifacts artifacts: '**/target/*.jar'
+            //}
+            //}
         }
     }

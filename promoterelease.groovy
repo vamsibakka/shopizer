3f751f940324c4522456d7a0ef85e8e6ec459209
vamsi <vamsikrishna.123.vk@gmail.com>
2022-10-20 11:19:26 +0530

    changed ssh link to http link and modified script

--- promoterelease.groovy
+++ promoterelease.groovy
@@ -13,24 +13,13 @@ pipeline {
             
                     sh 'git checkout develop'
                     sh 'git checkout master'
-                    hs 'git branch -a'
+                    sh 'git branch -a'
                     sh 'git checkout release'
                     sh 'git merge develop --no-ff'
-                    sh 'git log -3 --oneline'
+                    sh 'git push -u orogin release'
                 
-                // sh   // merging the git develop branch to release branch without fastfarward.
-                //sh 'mvn clean package'
+                
             }
         }
-        //stage ('junit_testresults') {
-         //   steps {
-           //     junit '**/surefire-reports/*.xml'
-            //}
-        //}
-        //stage ('archives') {
-          //  steps{
-            //    archiveArtifacts artifacts: '**/target/*.jar'
-            //}
-            //}
-    }
+        }
 }
